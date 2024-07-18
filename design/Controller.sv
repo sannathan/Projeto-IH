d4cc2c5603ea8a29052955a21baee2391ec2c50a
sannathan <nathansantos1713@gmail.com>
2024-07-18 10:19:47 -0300

    Controller finalizado

--- design/Controller.sv
+++ design/Controller.sv
@@ -19,19 +19,25 @@ module Controller (
     output logic Branch  //0: branch is not taken; 1: branch is taken
 );
 
-  logic [6:0] R_TYPE, LW, SW, BR;
+  logic [6:0] R_TYPE, B_TYPE, I_TYPE, S_TYPE, JAL, JALR, SHIFT_TYPE; //Adicionar mais vetores
 
-  assign R_TYPE = 7'b0110011;  //add,and
-  assign LW = 7'b0000011;  //lw
-  assign SW = 7'b0100011;  //sw
-  assign BR = 7'b1100011;  //beq
+  assign R_TYPE = 7'b0110011;  //add,and, sub, slt, xor, or
+  assign B_TYPE = 7'b1100011; //beq, bne, blt, bge
+  assign I_TYPE = 7'0010011; //slti, addi, slli, srli, srai obs: vai precisar fazer calculo de deslocamento quando for usar
+  assign S_TYPE = 7'b0100011; // sw, sb, sh
+  assign LOAD_TYPE = 7'b0000011; //lw, lb, lh, lbu 
+  assign JAL = 7'b1101111; // jal
+  assign JALR = 7'b1100111; // jalr
+  
+  //Nao ha opcode padrao para HALT
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW);
-  assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW);
-  assign MemRead = (Opcode == LW);
-  assign MemWrite = (Opcode == SW);
-  assign ALUOp[0] = (Opcode == BR);
+  assign ALUSrc = (Opcode == I_TYPE || Opcode == S_TYPE || Opcode == LOAD_TYPE);
+  assign MemtoReg = (Opcode == LOAD_TYPE);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == I_TYPE || Opcode == LOAD_TYPE);
+  assign MemRead = (Opcode == LOAD_TYPE);
+  assign MemWrite = (Opcode == S_TYPE);
+  assign ALUOp[0] = (Opcode == B_TYPE);
   assign ALUOp[1] = (Opcode == R_TYPE);
-  assign Branch = (Opcode == BR);
+  assign Branch = (Opcode == B_TYPE);
+  
 endmodule
