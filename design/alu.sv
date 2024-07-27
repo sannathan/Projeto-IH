7a8212230a90e98ffda8d6990443ced8954df113
MatheusMenezesss <159057394+MatheusMenezesss@users.noreply.github.com>
2024-07-27 18:50:55 -0300

    Update alu.sv

--- design/alu.sv
+++ design/alu.sv
@@ -14,16 +14,41 @@ module alu#(
     
         always_comb
         begin
-            case(Operation)
-            4'b0000:        // AND
-                    ALUResult = SrcA & SrcB;
-            4'b0010:        // ADD
-                    ALUResult = SrcA + SrcB;
-            4'b1000:        // Equal
-                    ALUResult = (SrcA == SrcB) ? 1 : 0;
-            default:
-                    ALUResult = 0;
-            endcase
+                case(Operation)
+                4'b0000:        // AND, ANDI
+                        ALUResult = SrcA & SrcB;
+                4'b0001:        // OR, ORI
+                        ALUResult = SrcA | SrcB;
+                4'b0010:        // XOR, XORI
+                        ALUResult = SrcA ^ SrcB;
+                4'b0011:        // LOAD, STORE, ADD, ADDI
+                        ALUResult = $signed(SrcA) + $signed(SrcB);
+                4'b0100:        // SUB
+                        ALUResult = $signed(SrcA) - $signed(SrcB);
+                4'b0101:        // SRL, SRLI
+                        ALUResult = SrcA >> SrcB;
+                4'b0110:        // SRA, SRAI
+                        ALUResult = $signed(SrcA) >>> $signed(SrcB);
+                4'b0111:        // SLL, SLLI
+                        ALUResult = SrcA << SrcB;
+                4'b1000:        // BEQ
+                        ALUResult = (SrcA == SrcB) ? 1 : 0;
+                4'b1001:        // BNE
+                        ALUResult = (SrcA != SrcB) ? 1 : 0;
+                4'b1010:        // SLT, SLTI, BLT
+                        ALUResult = (SrcA < SrcB) ? 1 : 0;
+                4'b1011:        // BGE
+                        ALUResult = (SrcA >= SrcB) ? 1 : 0;
+                4'b1100:        // JALR
+                        ALUResult = SrcA;
+                4'b1101:        // LUI
+                        ALUResult = SrcB;
+                4'b1110:        // BLTU, SLTU, SLTIU
+                        ALUResult = ($unsigned(SrcA) < $unsigned(SrcB)) ? 1 : 0;
+                4'b1111:        // BGEU
+                        ALUResult = ($unsigned(SrcA) >= $unsigned(SrcB)) ? 1 : 0;
+                default:
+                        ALUResult = 0;
+                endcase
         end
 endmodule
-
