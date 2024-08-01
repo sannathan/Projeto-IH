257f7264296d5c28034f8abf7613a560c6d401de
sannathan <nathansantos1713@gmail.com>
2024-08-01 14:04:24 -0300

    Troca de instruction para inst_code e de ImmOut para Imm_out

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -19,34 +19,34 @@ module imm_Gen (
 
         endcase
       7'b0000011:  /*I-type load part*/
-      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+        Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
 
       7'b0100011:  /*S-type*/
-      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:25], inst_code[11:7]};
+        Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:25], inst_code[11:7]};
 
       7'b1100011:  /*B-type*/
-      Imm_out = {
-        inst_code[31] ? 19'h7FFFF : 19'b0,
-        inst_code[31],
-        inst_code[7],
-        inst_code[30:25],
-        inst_code[11:8],
-        1'b0
-      };
-      7'b1101111: /*JAL*/
-        ImmOut = {
-          Instruction[31] ? 11'h7FF : 11'b0,
-          Instruction[31],
-          Instruction[19:12],
-          Instruction[20],
-          Instruction[30:21],
+        Imm_out = {
+          inst_code[31] ? 19'h7FFFF : 19'b0,
+          inst_code[31],
+          inst_code[7],
+          inst_code[30:25],
+          inst_code[11:8],
           1'b0
         };
-        7'b1100111: /*JALR*/
-          ImmOut = {
-            Instruction[31] ? 20'hFFFFF : 20'b0,
-            Instruction[31:20]
-          };
+      7'b1101111: /*JAL*/
+        Imm_out = {
+          inst_code[31] ? 11'h7FF : 11'b0,
+          inst_code[31],
+          inst_code[19:12],
+          inst_code[20],
+          inst_code[30:21],
+          1'b0
+        };
+      7'b1100111: /*JALR*/
+        Imm_out = {
+          inst_code[31] ? 20'hFFFFF : 20'b0,
+          inst_code[31:20]
+        };
 
       default: Imm_out = {32'b0};
 
